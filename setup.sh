df5863490a006d156520b14d5a64c72333f84d68
Takafumi Kimura <10005188+takafumi-kimura@users.noreply.github.com>
2020-10-18 08:39:56 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
+# anyenvインストール
 brew install anyenv
 anyenv install --init --force-init
 exec $SHELL -l
 anyenv install nodenv
 anyenv install rbenv
 exec $SHELL -l
+mkdir -p $(anyenv root)/plugins
+git clone https://github.com/znz/anyenv-update.git $(anyenv root)/plugins/anyenv-update
+
+# nodenvインストール
+mkdir -p "$(nodenv root)"/plugins
+git clone https://github.com/nodenv/nodenv-default-packages.git $(nodenv root)/plugins/nodenv-default-packages
+
+# 設定ファイル読み込み
+# TODO
+# 設定ファイル -> $(nodenv root)/default-packages
